6dbdc619ff7fdd4cb9c9e8d82812f44860b0b65c
geffrys <geffry.ospina@gmail.com>
2025-06-24 22:09:04 -0500

    base del proyecto backend

---
+++ nopmocounter/src/test/java/com/geff/nopmocounter/NopmocounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geff.nopmocounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NopmocounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
